92a7064de1003c6441beaeb4a6926923d11e21bc
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2025-03-06 11:10:45 +0500

    sign up page is completed, deployment error (hyderation) is solved.

--- src/app/(auth)/sign-In/page.tsx
+++ src/app/(auth)/sign-In/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+"use client"
+import React ,{useState,useEffect} from 'react'
+import Image from  'next/image';
+import SignIn from '@/components/SignIn'
+import signinImg from '@/constants/SigninImg.png'
 const SigninPage = () => {
+  // ======================================================
+  // Loader
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+   setLoading(true);
+  }, [])
+
+  if(!loading){
+    return(
+    <>
+      <h1>Loading...</h1>
+    </>
+    );
+  }
+// ==========================================================
+
+// Sign In page
   return (
-    <div>
-      Sign page
+    <div className='w-full h-full  flex justify-around items-center'>
+      <div className='w-[50%] h-full flex flex-col justify-center items-center'>
+        <h1></h1>
+        <p></p>
+       <span></span>
+       <div className='w-[25rem] h-[18.75rem] mb-2 items-center ml-0 mt-[2rem]'>
+         <Image src={signinImg} alt="page image"  
+          width={400} 
+          height={300} 
+          className="" />
+        </div> 
+      </div>
+      <div className='w-[50%] h-full items-center'>
+      <SignIn />
+      </div>
     </div>
   )
 }
